f14c9c2529f6c5a2759abe171a227615a7e0259a
imhjnoh <imhjnoh@gmail.com>
2021-03-11 00:38:18 +0900

    travis test 4

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,3 +6,8 @@ import App from './App';
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+test('renders learn react link', () => {
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
